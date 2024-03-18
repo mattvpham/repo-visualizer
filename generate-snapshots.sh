7a09b8272a940afb2da728e61940bae849fe0ff7
Matt Pham <matt@nava.io>
2024-03-18 02:10:54 -0700

    Create iterator script
    To be used like `./generate-snapshots.sh ~/code/platform`

---
+++ generate-snapshots.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -euo pipefail
+
+currentdir=$(pwd)
+targetdir=$1
+
+cd $targetdir
+
+# Get all commit hashes with [ETD-*] in the commit message
+commits=$(git log --pretty=format:'%H' --grep='\[ETD-.*\]')
+
+counter=1
+# Loop over each commit
+for commit in $commits
+do
+    # Checkout the commit
+    git checkout $commit
+
+    # Get the commit message
+    message=$(git log -1 --pretty=format:'%s' $commit)
+
+    # Print the commit message and counter
+    echo "Commit $counter: $message"
+
+    # Run your command here
+    echo "Running command on commit $commit"
+    # Replace the above echo command with your actual command
+
+    # Increment counter
+    ((counter++))
+done
+
+# Checkout the original branch after running the commands
+git checkout main
